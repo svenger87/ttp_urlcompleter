f1a77be06aaffa729eb990cb21d66a3390f80157
Sven Rosema <srosema@ttp-papenburg.de>
2023-08-29 13:56:19 +0200

    Add recents

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ttp_urlcompleter/main.dart';
+import 'package:ttp_urlcompleter/main.dart.bak';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
