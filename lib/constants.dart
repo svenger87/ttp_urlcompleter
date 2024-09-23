2bacf5b60d183797eccfefbc528d01e0bd31ed5b
Sven Rosema <srosema@ttp-papenburg.de>
2024-09-23 11:49:38 +0200

    Rename intranet hostname

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,15 +2,15 @@ const String ikOfficeLineConfig =
     'http://ikoffice.sip.local:8080/ikoffice/root/projektverwaltung/linienkonfiguration';
 const String ikOfficePZE = 'http://ikoffice.sip.local:8080/ikoffice/root/';
 const String prodPlan1w =
-    'http://lurchiweb.sip.local/schedule/ZPPLAN.pdf#view=FitH';
+    'http://ttp-connect.sip.local/schedule/ZPPLAN.pdf#view=FitH';
 const String prodPlan3w =
-    'http://lurchiweb.sip.local/schedule/ZPPLAN_3W.pdf#view=FitH';
+    'http://ttp-connect.sip.local/schedule/ZPPLAN_3W.pdf#view=FitH';
 const String prodPlan1wNextcloud =
     'https://wim-solution.sip.local:8443/s/iBbZrtda7BTT7Qp';
 const String prodPlan3wNextcloud =
     'https://wim-solution.sip.local:8443/s/EWxYDYmtKJQ2mfm';
 const String bookstack = 'http://bookstack.sip.local';
-const String intranet = 'http://lurchiweb.sip.local';
+const String intranet = 'http://ttp-connect.sip.local';
 const String ac = 'https://olymp.sip.de';
 const String wim = 'https://wim-solution.sip.local:8081';
 const String picklist = 'https://wim-solution.sip.local:8443/s/mYYc2cJyWG795BM';
@@ -19,6 +19,6 @@ const String apiUrl =
     'https://wim-solution.sip.local:8081/rest/v2/short-urls?itemsPerPage=10000';
 const String apiKey = 'b2380a66-c965-4177-8bbf-6ecf03fbaa32';
 const String matplan =
-    'http://lurchiweb.sip.local/schedule/MPlan.pdf#view=FitH';
+    'http://ttp-connect.sip.local/schedule/MPlan.pdf#view=FitH';
 const String matplanNextcloud =
     'https://wim-solution.sip.local:8443/s/rp8aedp2iaMerFD';
