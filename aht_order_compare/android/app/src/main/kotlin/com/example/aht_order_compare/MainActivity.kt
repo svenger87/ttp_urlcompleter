eac749af24895cf62750a193f0ee93080639fd53
Sven Rosema <rosema.sven@gmail.com>
2025-01-29 12:39:04 +0100

    Implement initial aht_order_compare

---
+++ aht_order_compare/android/app/src/main/kotlin/com/example/aht_order_compare/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aht_order_compare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ aht_order_compare/lib/constants/constants.dart
@@ -0,0 +1,4 @@
+// lib/constants/constants.dart
+
+// API Configuration
+const String kApiUrl = 'http://wim-solution.sip.local:5002/compare';
