c24444fcb6ad5740d20d37158f2b4f71ca33eed2
Sven Rosema <srosema@ttp-papenburg.de>
2025-02-05 14:12:16 +0100

    Some deps

--- tool_planning/.ios/Flutter/flutter_export_environment.sh
+++ tool_planning/.ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\rosema\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\rosema\Documents\Coding\ttp_urlcompleter\tool_planning"
+export "FLUTTER_ROOT=/Users/svenrosema/Documents/Code/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/svenrosema/Documents/Code/ttp_urlcompleter/tool_planning"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
